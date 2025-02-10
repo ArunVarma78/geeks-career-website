0128777d118e68d8e825901eb2c369f4bbe99298
ArunVarma1 <23497730-ArunVarma1@users.noreply.replit.com>
2025-02-10 10:41:53 +0000

    User checkpoint: Add job listings to website; includes frontend and backend changes.

--- config/db.py
+++ config/db.py
@@ -1,5 +1,3 @@
-from flask import Flask, render_template
-
 JOBS = [{
     "id": 1,
     "title": "Data Analyst",
@@ -11,14 +9,3 @@ JOBS = [{
     "location": "Delhi, India",
     "salary": "Rs. 15,00,000"
 }]
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-  return render_template('index.html', jobs=JOBS)
-
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
