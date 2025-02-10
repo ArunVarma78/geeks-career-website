9cbc34c8f01818a89192ff551da315bd8c7cca11
Arun Varma <159011947+ArunVarma78@users.noreply.github.com>
2025-02-10 10:26:15 +0000

    added navbar

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('index.html')
 
 
 if __name__ == "__main__":
-  app.debug = True
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5000, debug=True)
