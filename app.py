f0f9ae8b18ab3593d4d761a656242281483e57ae
Arun Varma <159011947+ArunVarma78@users.noreply.github.com>
2025-02-10 11:42:12 +0000

    added jobs section

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from config.db import JOBS
 
 app = Flask(__name__)
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   return render_template('index.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
